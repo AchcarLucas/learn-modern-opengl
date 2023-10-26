e527b763cd551a6824d2c0d7f8ca3e0203259d52
Lucas Campos <lucas.campos@ambrait.com>
2023-10-26 09:36:49 -0300

    - OpenGL with GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL with GLFW", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+{
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
     return 0;
 }
