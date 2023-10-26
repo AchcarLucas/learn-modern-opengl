0669bed8a8d2e4e154f8d5dcda3e084f4373e57d
Lucas Campos <lucas.campos@ambrait.com>
2023-10-26 09:37:42 -0300

    - ajuste do código main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,27 @@ using namespace std;
 int main()
 {
     glfwInit();
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL with GLFW", NULL, NULL);
+
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
 
     while(!glfwWindowShouldClose(window))
-{
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-}
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     return 0;
 }
