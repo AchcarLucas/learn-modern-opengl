70d20e7e2e34f5cfd87b17b8fe1b2035dc09c3a2
Lucas Campos <lucas.campos@ambrait.com>
2023-11-15 16:27:56 -0300

    - removendo o MSAA padrão do GLWF

--- project/src/init.cpp
+++ project/src/init.cpp
@@ -11,7 +11,7 @@ void initOpenGL(const int width, const int height)
 {
     glfwInit();
 
-    glfwWindowHint(GLFW_SAMPLES, 4);
+    //glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
