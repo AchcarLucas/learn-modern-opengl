0b894f88f72d9bc78cbf7e02d9d3907f98148463
Lucas Campos <lucas.campos@ambrait.com>
2023-12-04 15:11:59 -0300

    - pointLight na classe light

--- project/include/light.hpp
+++ project/include/light.hpp
@@ -5,11 +5,15 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <vector>
+
 class Light
 {
     public:
         Light(glm::vec3 position);
         virtual ~Light();
+
+        glm::vec3 getPosition() { return position; }
 
     protected:
         glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
@@ -43,8 +47,30 @@ class DirectionalLight : public Light
         glm::vec3 direction;
 
     private:
-        glm::mat4 projection;
-        glm::mat4 view;
+};
+
+class PointLight : public Light
+{
+    public:
+        PointLight(const glm::vec3 position, const int width, const int height, const float near, const float far);
+        virtual ~PointLight();
+
+        void setPosition(glm::vec3 position) { this->position = position; }
+
+        glm::mat4 getProjectionMatrix();
+        std::vector<glm::mat4> getTransformMatrix();
+
+        float getNearPlane() { return this->near_plane; }
+        float getFarPlane() { return this->far_plane; }
+
+    protected:
+        int width;
+        int height;
+
+        float near_plane;
+        float far_plane;
+
+    private:
 };
 
 #endif // LIGHT_HPP
